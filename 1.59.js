b891b876f9793121b3dd633504acace454065712
Chernik1991 <5059585@mail.ru>
2022-02-06 13:13:21 +0300

    1.59

---
+++ 1.59.js
@@ -0,0 +1,10 @@
+function trueOrFalse(wasThatTreu) {
+    // Only change code below this line
+    if (wasThatTreu) {
+        return "Yes, that was true";
+    }
+    return "No, that was false";
+
+    // Only change code above this line
+
+}
